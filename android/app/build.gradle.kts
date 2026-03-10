a5be8e41ab054456a56cbdc0de8b71bc360d8d24
Denys Vitali <denys@denv.it>
2026-03-10 19:03:22 +0000

    Use it.denv app namespace

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.argocd_flutter"
+    namespace = "it.denv.argocd_flutter"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.argocd_flutter"
+        applicationId = "it.denv.argocd_flutter"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
--- android/app/src/main/kotlin/it/denv/argocd_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/it/denv/argocd_flutter/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.argocd_flutter
+package it.denv.argocd_flutter
 
 import io.flutter.embedding.android.FlutterActivity
 
